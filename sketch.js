b1eaeb2481cc69526b0e984a4e4a25f7f99f84e3
LongLivetheReed <87645804+LongLivetheReed@users.noreply.github.com>
2022-01-17 18:57:41 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var weight = [35,37,39,41];
+
+function setup() 
+{
+  createCanvas(400,400);
+var sum = weight[0]+weight[1]+weight[2]+weight[3];
+var average = sum/weight.length;
+console.log(average)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
